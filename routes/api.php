1118dfff31fa7d3ed33dd9440a5f0f8e713ae93e
Bede Abbe <bede.abbe@gmail.com>
2024-11-16 23:44:02 +0000

    url changes

--- routes/api.php
+++ routes/api.php
@@ -7,23 +7,23 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-
-// Route::get('/number-category-validation/{number}', [App\Http\Controllers\NumberValidationApiController::class, 'checkNumberCategory']);
-Route::get('/existing-number/{number}', [App\Http\Controllers\NumberValidationApiController::class, 'checkNumberExist']);
-Route::post('/verify-pin', [App\Http\Controllers\AuthApiController::class, 'validatePin']);
-// Route::post('/reset-pin', [App\Http\Controllers\AuthApiController::class, 'resetPin']);
-// Route::post('/change-pin', [App\Http\Controllers\AuthApiController::class, 'changePin']);
-Route::post('/reset-pin', [App\Http\Controllers\AuthApiController::class, 'changePin']);
-Route::post('/list-accounts', [App\Http\Controllers\AccountsApiController::class, 'listAccountsFirst3']);
-Route::post('/list-cust-accounts', [App\Http\Controllers\AccountsApiController::class, 'listAccountsFirst3Cust']);
-Route::post('/credit-account', [App\Http\Controllers\AccountsApiController::class, 'creditAccount']);
-Route::post('/account-balance', [App\Http\Controllers\AccountsApiController::class, 'checkAccountBalance']);
-Route::post('/get-contact-info', [App\Http\Controllers\AccountsApiController::class, 'getContactInfo']);
-Route::post('/field-deposit', [App\Http\Controllers\AccountsApiController::class, 'saveFieldDeposit']);
-Route::post('/register-customer', [App\Http\Controllers\AuthApiController::class, 'registerAccount']);
-Route::post('/create-field-account', [App\Http\Controllers\AuthApiController::class, 'createFieldAccount']);
-Route::get('/name-inquiry/{number}', [App\Http\Controllers\NumberValidationApiController::class, 'nameInquiryAccountNumber']);
-
+Route::prefix('889901')->group(function () {
+    // Route::get('/number-category-validation/{number}', [App\Http\Controllers\NumberValidationApiController::class, 'checkNumberCategory']);
+    Route::get('/existing-number/{number}', [App\Http\Controllers\NumberValidationApiController::class, 'checkNumberExist']);
+    Route::post('/verify-pin', [App\Http\Controllers\AuthApiController::class, 'validatePin']);
+    // Route::post('/reset-pin', [App\Http\Controllers\AuthApiController::class, 'resetPin']);
+    // Route::post('/change-pin', [App\Http\Controllers\AuthApiController::class, 'changePin']);
+    Route::post('/reset-pin', [App\Http\Controllers\AuthApiController::class, 'changePin']);
+    Route::post('/list-accounts', [App\Http\Controllers\AccountsApiController::class, 'listAccountsFirst3']);
+    Route::post('/list-cust-accounts', [App\Http\Controllers\AccountsApiController::class, 'listAccountsFirst3Cust']);
+    Route::post('/credit-account', [App\Http\Controllers\AccountsApiController::class, 'creditAccount']);
+    Route::post('/account-balance', [App\Http\Controllers\AccountsApiController::class, 'checkAccountBalance']);
+    Route::post('/get-contact-info', [App\Http\Controllers\AccountsApiController::class, 'getContactInfo']);
+    Route::post('/field-deposit', [App\Http\Controllers\AccountsApiController::class, 'saveFieldDeposit']);
+    Route::post('/register-customer', [App\Http\Controllers\AuthApiController::class, 'registerAccount']);
+    Route::post('/create-field-account',    [App\Http\Controllers\AuthApiController::class, 'createFieldAccount']);
+    Route::get('/name-inquiry/{number}', [App\Http\Controllers\NumberValidationApiController::class, 'nameInquiryAccountNumber']);
+});
 // curl -ivk http://127.0.0.1:8001/api/verify-pin
 // --header 'Content-Type: application/json' \
 // --data-raw '{
