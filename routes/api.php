206d1189a49548821a30055bae8ed7e62d6cdcf4
Terminal-Adawe <bede.abbe91@gmail.com>
2026-03-02 22:09:41 +0000

    changes

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,9 @@ Route::prefix(config('customConfig.clientId'))->group(function () {
     Route::get('/fetch-approved-accounts', [App\Http\Controllers\CoopsController::class, 'fetchApprovedAccounts']);
     Route::post('/activate-verified-customer', [App\Http\Controllers\CoopsController::class, 'activateVerifiedCustomer']); 
     Route::get('/account-statement/{accountNumber}', [App\Http\Controllers\AccountsApiController::class, 'accountStatement']);
+    Route::get('/list-account-loans/{accountNumber}', [App\Http\Controllers\AccountsApiController::class, 'listAccountLoans']);
+    Route::get('/loan-repayment', [App\Http\Controllers\AccountsApiController::class, 'loanRepayment']);
+    Route::get('/account-statement/{accountNumber}', [App\Http\Controllers\AccountsApiController::class, 'accountStatement']);
 });
 // curl -ivk http://127.0.0.1:8001/api/verify-pin
 // --header 'Content-Type: application/json' \
